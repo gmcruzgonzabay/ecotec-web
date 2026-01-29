cbdc99b0429ff7f4381bc88507cf04148b9c8bcb
gmcruzgonzabay <proyectosapp@supernet.com.ec>
2026-01-28 20:24:05 -0500

    class

--- sesiones/Sesion-06/script.js
+++ sesiones/Sesion-06/script.js
@@ -1,6 +1,17 @@
-let numButtonClicks = 0;
-function buttonClicked() {
-    numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
-}
+//Inicio de la clase persona
+class Persona{
+ constructor(nombre,edad)
+    {
+        this.nombre=nombre;
+        this.edad=edad;
+    }
+    info(){
+        return `Nombre:${this.nombre}, Edad: ${this.edad}`;
+    }
+}// Fin de la clase persona
+
+//llamo al evento click del boton
+    document.getElementById("btn").addEventListener("click",()=>{
+        const p= new Persona("Ana",25);
+        document.getElementById("resultado").innerText=p.info();   
+    });
