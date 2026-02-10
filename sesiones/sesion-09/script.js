23b25f1d3036a6c450e290bea10b322030af4afa
gmcruzgonzabay <proyectosapp@supernet.com.ec>
2026-02-09 19:17:45 -0500

    Sesion-09

--- sesiones/sesion-09/script.js
+++ sesiones/sesion-09/script.js
@@ -3,47 +3,39 @@ document.addEventListener("DOMContentLoaded", function () {
     //declaraciones globales
     const btnCargar = document.getElementById("btnCargar");
     const resultado = document.getElementById("resultado");
-    const ordenarAZ=document.getElementById("ordenarAZ");
-    const ordenarZA=document.getElementById("ordenarZA");
+    const ordenarAZ = document.getElementById("ordenarAZ");
+    const ordenarZA = document.getElementById("ordenarZA");
     //Para la búsqueda
-    let personasGlobal=[];
-    const buscar=document.getElementById("buscar");
-     
+    let personasGlobal = [];
+    const buscar = document.getElementById("buscar");
 
-    //Eventos 
+    //Eventos
     btnCargar.addEventListener("click", cargarPersonas);
-    buscar.addEventListener("keyup",filtrarPersonas);
-    ordenarAZ.addEventListener("click",ordenarNombreAZ);
-    ordenarZA.addEventListener("click",ordenarNombreZA);
-    
-    
+    buscar.addEventListener("keyup", filtrarPersonas);
+    ordenarAZ.addEventListener("click", ordenarNombreAZ);
+    ordenarZA.addEventListener("click", ordenarNombreZA);
+
     //Buscar por nombre
-    function filtrarPersonas(){
+    function filtrarPersonas() {
         console.log("Ingresa a Filtrar Persona");
-        const texto=buscar.value.toLowerCase();
-        const personasFiltradas=personasGlobal.filter((persona)=>
-        persona.nombre.toLowerCase().includes(texto) || persona.ciudad.toLowerCase().includes(texto) );
-    mostrarPersonas(personasFiltradas);
+        const texto = buscar.value.toLowerCase();
+        const personasFiltradas = personasGlobal.filter(
+            (persona) => persona.nombre.toLowerCase().includes(texto) || persona.ciudad.toLowerCase().includes(texto)
+        );
+        mostrarPersonas(personasFiltradas);
     }
     //Ordena de A-Z
-    function ordenarNombreAZ(){
-        personasGlobal.sort((a,b)=>a.nombre.localeCompare(b.nombre));
+    function ordenarNombreAZ() {
+        personasGlobal.sort((a, b) => a.nombre.localeCompare(b.nombre));
         mostrarPersonas(personasGlobal);
     }
-    
+
     //Ordena de Z-A
-        function ordenarNombreZA(){
-        personasGlobal.sort((a,b)=>b.nombre.localeCompare(a.nombre));
+    function ordenarNombreZA() {
+        personasGlobal.sort((a, b) => b.nombre.localeCompare(a.nombre));
         mostrarPersonas(personasGlobal);
     }
-   function eliminarPersona(email){
-        const confirmar=confirm("Seguro desea eliminar?");
-        if(!confirmar)return;
-        personasGlobal=personasGlobal.filter((persona)=>persona.email!== email);
-        mostrarPersonas(personasGlobal);
-        document.getElementById("Total").innerText=`Total de usuarios ${personasGlobal.length}`;
-        
-    }
+
     class Persona {
         //Inicializo los atributos de la clase
         constructor(nombre, email, ciudad, empresa) {
@@ -66,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         try {
             //Simular el tiempo de espera
-          await  esperar(2000);
+            await esperar(1000);
             resultado.innerText = "Cargando datos......";
             const response = await fetch("https://jsonplaceholder.typicode.com/users");
             if (!response.ok) {
@@ -82,8 +74,8 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log(total);
             console.log(totalpersonas);
             document.getElementById("total").innerText = `Total de usuarios:${data.length}`;
-            personasGlobal=personas;
- 
+            personasGlobal = personas;
+
             //Cambio en las fechas del examen parcial
             //del 6 de febrero a las 19:00 al 8 de Febrero 23:00
 
@@ -92,16 +84,16 @@ document.addEventListener("DOMContentLoaded", function () {
             resultado.innerText = "Error al consultar los datos";
             console.error(error);
         } finally {
-           // console.log("Ingresa al bloque del finally");
+            // console.log("Ingresa al bloque del finally");
             document.getElementById("spinner").classList.add("d-none");
             btnCargar.disabled = false;
-        }//final finally
+        } //final finally
     } // fin de la funcion async cargarPersonas
 
     function mostrarPersonas(personas) {
         const tbody = document.querySelector("#tablaPersonas tbody ");
         tbody.innerHTML = "";
- 
+
         personas.forEach((persona) => {
             const tr = document.createElement("tr");
             tr.innerHTML = `
@@ -109,20 +101,25 @@ document.addEventListener("DOMContentLoaded", function () {
                 <td>${persona.email}</td>
                 <td>${persona.ciudad}</td> 
                 <td>${persona.empresa}</td>
-                <td><button class="btn btn-sm btn-danger">Eliminar</button></td>
+                <button class="btn btn-sm btn-danger">Eliminar</button>
+               
                 `;
             tbody.appendChild(tr);
         });
-        const btnEliminar=tr.querySelector("button");
-        btnEliminar.addEventListener("click",()=>{
-            eliminarPersona(persona.email);
-        }); 
 
         resultado.innerText = "";
     }
-    
- 
 
+    //Funcion para eliminar un registro
+    function eliminarPersona(email) {
+        const confirmar = confirm("¿Está seguro de eliminar este usuario?");
+        if (!confirmar) return;
+        personasGlobal = personasGlobal.filter((persona) => persona.email !== email);
+        mostrarPersonas(personasGlobal);
+        document.getElementById("total").innerText = `Total de usuarios: ${personasGlobal.length}`;
+    }//Fin funcion de eliminar
+
+    //Simulo una espera
     function esperar(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
     }
